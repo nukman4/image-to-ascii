382aee56229854ced88bcdc036be662dc44743c5
nukman4 <101593515+nukman4@users.noreply.github.com>
2023-05-09 08:41:32 -0600

    Update colored.py

--- colored.py
+++ colored.py
@@ -1,8 +1,9 @@
 from PIL import Image
+
 import urllib.request
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
-  
+
 print(colored(255, 0, 0, 'Hello, World'))
 pic = ""
 pic1 =""
@@ -29,16 +30,16 @@ img = img.convert("RGB")
 pixel_map = img.load()
 h, w = img.size
 for i in range(w):
+  pic1 = ""
   for j in range(h):
     r, g, b = img.getpixel((j, i))
-    avg = int((0.299*r + 0.587*g + 0.114*b))
-    text = '0'
-    colored_text = colored(avg, avg, avg, text)
-    pic += colored_text
+    #avg = int((0.299*r + 0.587*g + 0.114*b))
+    #text = '0'
+    #colored_text = colored(avg, avg, avg, text)
+    #pic += colored_text
     text = '0'
     colored_text = colored(r, g, b, text)
     pic1+=colored_text
-  pic += "\n"
-  pic1 +="\n"
-#print(pic)
-print(pic1)
+
+  pic1 += "\n"
+  print(pic1,end="")
